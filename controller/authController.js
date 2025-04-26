aa14586b1d7968f158aa7958ac368e973c74b8c0
ebaze-tech <ebaze.technologies@gmail.com>
2025-04-26 02:42:10 +0100

    third update

--- controller/authController.js
+++ controller/authController.js
@@ -15,7 +15,7 @@ const adminAuthController = async (req, res) => {
     }
 
     try {
-        const user = await Admin.findOne({ email }).select('+password')
+        const user = await Admin.findOne({ email })
         if (!user) {
             console.log("Invalid email")
             return res.status(400).json({ message: "Invalid email" })
@@ -40,6 +40,54 @@ const adminAuthController = async (req, res) => {
     } catch (error) {
         console.error("Login error:", error)
         return res.status(500).json({ message: "Login error: ", error })
+    }
+}
+
+const adminRegisterController = async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        console.log('All fields are required')
+        return res.status(400).json({ message: 'All fields are required' })
+    }
+
+    try {
+        const adminNumber = Math.floor(100000 + Math.random() * 900000)
+
+        const existingUserByEmail = await Admin.findOne({ email })
+        if (existingUserByEmail) {
+            console.log('Email is already in use')
+            return res.status(400).json({ message: 'Email used' })
+        }
+
+        // Hash password
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        // Create new user
+        const user = new Admin({
+            email,
+            password: hashedPassword,
+        })
+        await user.save()
+
+
+        // Generate JWT token
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1d'
+        })
+
+        console.log('Admin registered successfully:', { user, token })
+        res.status(201).json({
+            message: 'User registered successfully.',
+            user: {
+                id: user._id,
+                email: user.email
+            },
+            token
+        })
+    } catch (error) {
+        console.error('Registration error:', error)
+        res.status(500).json({ message: 'Error registering user', error })
     }
 }
 const userAuthController = async (req, res) => {
@@ -72,4 +120,5 @@ const userAuthController = async (req, res) => {
         return res.status(500).json({ message: "Login error:", error })
     }
 }
-module.exports = { adminAuthController, userAuthController }
+
+module.exports = { adminAuthController, adminRegisterController, userAuthController }
--- route/authRoute.js
+++ route/authRoute.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
-const { adminAuthController, userAuthController } = require("../controller/authController")
+const { adminAuthController, userAuthController, adminRegisterController } = require("../controller/authController")
 
 router.post("/admin/login", adminAuthController)
+router.post("/admin/register", adminRegisterController)
 router.post("/user/login", userAuthController)
 
 module.exports = router
+
