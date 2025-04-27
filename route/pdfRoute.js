9559a50040e4d14e8d4e37beb206c56237fd229d
ebaze-tech <ebaze.technologies@gmail.com>
2025-04-28 00:41:09 +0100

    final fix?

--- route/pdfRoute.js
+++ route/pdfRoute.js
@@ -6,6 +6,7 @@ const { authenticateUser, isAdmin } = require("../middleware/authMiddleware")
 
 router.post('/generate', authenticateUser, pdfController)
 router.get('/:formId/download', authenticateUser, pdfDownloadController)
+router.get('/admin/:formId/download', isAdmin, pdfDownloadController)
 router.put('/:formId/update', isAdmin, pdfUpdateController)
 
 module.exports = router
