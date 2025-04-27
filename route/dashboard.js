0212922877ce63104a963f94a4a0479a1e6fd028
ebaze-tech <ebaze.technologies@gmail.com>
2025-04-27 22:41:53 +0100

    updtes

--- route/dashboard.js
+++ route/dashboard.js
@@ -1,12 +1,18 @@
-const express = require("express")
-const router = express.Router()
-const { viewForms, editForm, viewSingleForm } = require("../controller/adminDashboardController")
-const { isAdmin, authenticateUser } = require("../middleware/authMiddleware")
+const express = require("express");
+const router = express.Router();
+const {
+  viewForms,
+  editForm,
+  viewSingleForm,
+  userForm,
+} = require("../controller/adminDashboardController");
+const { isAdmin, authenticateUser } = require("../middleware/authMiddleware");
 
-router.get("/all-forms", isAdmin, viewForms)
-router.get("/form/:formId", isAdmin, viewSingleForm)
-router.get("/form/student/:formId",authenticateUser,viewSingleForm)
-router.put("/edit-form", isAdmin, editForm)
+router.get("/all-forms", isAdmin, viewForms);
+router.get("/form/student", authenticateUser, userForm);
+router.get("/form/:formId", isAdmin, viewSingleForm);
+router.get("/form/view/:formId", authenticateUser, viewSingleForm);
+router.get("/form/student/:formId", authenticateUser, viewSingleForm);
+router.put("/edit-form", isAdmin, editForm);
 
-
-module.exports = router
+module.exports = router;
